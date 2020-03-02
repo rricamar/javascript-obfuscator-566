3c5a8446b8e10fa0f51ccf94bcf3cf444b9c6963
Ruben Rica <ruben.rica@soprasteria.com>
2020-03-02 09:59:42 +0100

    non-working example

---
+++ index.js
@@ -0,0 +1,23 @@
+async function handleImageUpload(event) {
+    const imageFile = event.target.files[0];
+
+    console.log(`originalFile size ${imageFile.size / 1024 / 1024} MB`);
+
+    const options = {
+        maxSizeMB: 1,
+        maxWidthOrHeight: 1920,
+        useWebWorker: true
+    };
+
+    try {
+        const compressedFile = await imageCompression(imageFile, options);
+        console.log(
+            `compressedFile size ${compressedFile.size / 1024 / 1024} MB`
+        ); // smaller than maxSizeMB
+
+        alert("success");
+    } catch (error) {
+        console.log(error);
+        alert("error");
+    }
+}
